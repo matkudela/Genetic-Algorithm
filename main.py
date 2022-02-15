d16dff9d0b89aea5756c031253f43f27f36fde99
matkudela <56888605+matkudela@users.noreply.github.com>
2022-02-15 19:21:54 +0100

    added creating initial population and roulette selection

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import random
 
+
 class TravellingSalesman:
     def __init__(self):
         # list with distances between cities
         self.__distances = []
         self.__number_of_cities = 0
+        self.__population = []
+        # size of population (number of individuals in population array)
+        self.__n = 40
+        # distance of every individual from population in one list
+        self.__calculated_paths = []
+
         self.__open_file_and_set_distances()
+        self.__generate_population()
+        self.__fill_calculated_paths()
+        self.__roulette_selection()
+
+    @property
+    def __population(self):
+        return self.population
+
+    @__population.setter
+    def __population(self, value):
+        self.population = value
 
     @property
     def __distances(self):
@@ -47,7 +65,7 @@ class TravellingSalesman:
                 continue
         return individual
 
-    #method that checks distance from one city to another in distances matrix
+    # method that checks distance from one city to another in distances matrix
     def __check_distance(self, first_city, second_city):
         return self.__distances[first_city][second_city]
 
@@ -60,13 +78,59 @@ class TravellingSalesman:
         distance += self.__check_distance(individual[-1], individual[0])
         return distance
 
+    def __generate_population(self):
+        for x in range(self.__n):
+            self.__population.append(self.__generate_individual())
+
+    # method that calculates distance of every individual in population and append it into list
+    def __fill_calculated_paths(self):
+        for x in range(len(self.__population)):
+            self.__calculated_paths.append(self.__calculate_distance(self.__population[x]))
+
+    def __roulette_selection(self):
+        # roulette selection - we want to select individuals for new population - the lower overall distance
+        # of individual the better (individuals with better distance will have higher chance to be rolled for next
+        # population)
+        # find max value
+        max_path = max(self.__calculated_paths)
+
+        # recalculate paths (to make lower value better)
+        for x in range(len(self.__calculated_paths)):
+            self.__calculated_paths[x] = max_path + 1 - self.__calculated_paths[x]
+
+        # calculate sum of distances
+        distances_in_total = 0
+        for x in range(len(self.__calculated_paths)):
+            distances_in_total += self.__calculated_paths[x]
+
+        # create ranges, they will be used for rolling, every individual has lower and upper bound
+        # eg. when first and second individuals have distances 9 and 31. First individual gets bound from 0 to 9
+        # second one from 9 to 40 (9+31) and so on...
+        ranges = []
+        lower_bound = 0
+        upper_bound = 0
+        for x in range(len(self.__calculated_paths)):
+            upper_bound += self.__calculated_paths[x]
+            tmp = [lower_bound, upper_bound]
+            ranges.append(tmp)
+            lower_bound = upper_bound
+
+        # now generate random number, when it will be between lower and upper-1 bound, individual with the same
+        # index will be copied to next population
+        new_population = []
+        for x in range(len(self.__population)):
+            generated = random.randint(0, distances_in_total)
+            for y in range(len(ranges)):
+                if ranges[y][0] <= generated < ranges[y][1]:
+                    index = y
+            new_population.append(self.__population[index])
+
+        self.__population = new_population
+
     def print_distances(self):
-        # for d in self.__distances:
+        pass
+        # for d in self.__population:
         #     print(d)
-        ind = self.__generate_individual()
-        print(ind)
-        print(self.__calculate_distance(ind))
 
 
 t1 = TravellingSalesman()
-t1.print_distances()
